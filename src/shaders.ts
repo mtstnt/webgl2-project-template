9d741a06516461d82d4d6dc9292e1ed9dac4685d
mtstnt <matthew.sutanto0612@gmail.com>
2024-01-16 17:01:21 +0700

    sofa removed LMAO 2

--- src/shaders.ts
+++ src/shaders.ts
@@ -5,8 +5,6 @@ export function buildShader(gl: WebGL2RenderingContext): WebGLProgram {
     const vs = gl.createShader(gl.VERTEX_SHADER)!;
     const fs = gl.createShader(gl.FRAGMENT_SHADER)!;
 
-    console.log("OBJ: " + sofaObj);
-
     gl.shaderSource(vs, vertexShader);
     gl.shaderSource(fs, fragmentShader);
 
